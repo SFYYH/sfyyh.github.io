8a415e2628808ca6ac44ef789e5edac3b44e9b10
SFYYH <3300519161@qq.com>
2024-06-23 16:57:21 +0800

    Site updated: 2024-06-23 16:57:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/23/LNMP环境准备/","2024/06/23/LNMP环境工作原理/","2024/06/23/Socket套接字/"];function toRandomPost(){
+var posts=["2024/06/23/LNMP环境准备/","2024/06/23/LNMP环境工作原理/","2024/06/23/MySql的几种安装方式/","2024/06/23/Socket套接字/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
