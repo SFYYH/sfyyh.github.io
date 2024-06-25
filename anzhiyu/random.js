dedef1cbd9ac85961c10d7542e44b38c9746e7ec
SFYYH <3300519161@qq.com>
2024-06-25 21:50:01 +0800

    Site updated: 2024-06-25 21:50:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/23/LNMP环境准备/","2024/06/23/LNMP环境工作原理/","2024/06/23/MySql的几种安装方式/","2024/06/23/Socket套接字/","2024/06/25/Docker/"];function toRandomPost(){
+var posts=["2024/06/23/LNMP环境准备/","2024/06/23/LNMP环境工作原理/","2024/06/23/MySql的几种安装方式/","2024/06/23/Socket套接字/","2024/06/25/Docker/","2024/06/25/Linux自有服务和软件包/","2024/06/25/Linux进程检测与空值/","2024/06/25/Mysql主从架构/","2024/06/25/Mysql数据库的备份与恢复/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
